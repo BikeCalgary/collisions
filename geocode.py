b528321fea23b786f0fdceb1dc3bad5017c081c4
meejah <meejah@meejah.ca>
2014-02-24 01:50:53 -0700

    more geocoding stuff, start of geojson outout script

--- geocode.py
+++ geocode.py
@@ -2,7 +2,8 @@
 
 ##
 ## this takes addresses, one per line (like from "to-geocode" output
-## of collisions.py) and provides lat/lng for them.
+## of collisions.py) and provides lat/lng for them in CSV style
+## output, like "address, lat, lng"
 ##
 ## we're using the open.mapquestapi.com batch geocoder which takes 100
 ## at a time.
@@ -10,14 +11,21 @@
 
 "http://www.mapquestapi.com/geocoding/v1/address?&key=YOUR-KEY-HERE&location=1555 Blake St,Denver,CO,80202"
 
+import csv
 from xml.dom import minidom
 import requests
 import sys
 
 if False:
-    print requests.get('http://geocoder.ca',
+    r0 = requests.get('http://geocoder.ca',
                        params=dict(addresst='52+ST+NE', stno='1440',
                                    city='calgary', prov='ab', geoit='XML')).text
+    print r0
+    r1 = requests.get('http://geocoder.ca',
+                      params=dict(addresst='16+AV+SE+&+36+ST+SE',# stno='1440',
+                                  city='calgary', prov='ab', geoit='XML')).text
+    print r1
+
     sys.exit(0)
 
 def quote(s):
@@ -25,42 +33,48 @@ def quote(s):
 
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.6; rv:25.0) Gecko/20100101 Firefox/24.0'}
 
-with open("to-geocode", "r") as f:
-    for line in f.readlines()[10:12]:
-        if '&' in line:
-            print "intersection", line
-            a, b = line.strip().split('&')
-            resp = requests.get('http://geocoding.ca', 
-                                params=dict(street1=quote(a),
-                                            street2=quote(b),
-                                            city='Calgary', prov='ab',
-                                            geoit='XML'),
-                                headers=headers)
+def get_lat_lng(line):
+    if '&' in line:
+        resp = requests.get('http://geocoder.ca',
+                            params=dict(addresst=line.replace(' ', '+'),
+                                        city='calgary', prov='ab', geoit='XML')).text
+    else:
+        number, rest = line.split(' ', 1)
+        resp = requests.get('http://geocoder.ca',
+                            params=dict(addresst=rest.replace(' ', '+'), stno=number,
+                                        city='calgary', prov='ab', geoit='XML')).text
 
-        else:
-            num, rest = line.split(' ', 1)
-            try:
-                num = int(num)
-                rest = quote(rest)
-                resp = requests.get('http://geocoding.ca', 
-                                    params=dict(addresst=quote(rest), stno=str(num),
-                                                city='calgary', prov='ab', geoit='XML'),
-                                    headers=headers)
-            except ValueError:
-                addr = urllib2.quote(line.strip())
-                resp = requests.get('http://geocoding.ca', 
-                                    params=dict(addresst=rest, stno=str(num),
-                                                city='calgary', prov='ab', geoit='XML'),
-                                    headers=headers)
+    dom = minidom.parseString(resp)
+    lat = dom.getElementsByTagName('latt')[0].firstChild.wholeText
+    lng = dom.getElementsByTagName('longt')[0].firstChild.wholeText
+    
+    return lat, lng
 
-        if resp.status_code == 200:
-            data = resp.text
-            print data
-            dom = minidom.parseString(data)
-            lat = dom.getElementsByTagName('latt')[0].firstChild.wholeText
-            lng = dom.getElementsByTagName('longt')[0].firstChild.wholeText
+pre_existing = {}
+try:
+    with open('geocoded.csv', 'r') as exist_file:
+        for (addr, lat, lng) in csv.reader(exist_file):
+            pre_existing[addr] = (lat, lng)
 
-            print line.strip(), lat, lng
-        else:
-            print "ERROR", resp.status_code, resp.reason, line,
-            
+except IOError:
+    print "nothing pre-existing"
+
+with open('geocoded.csv', 'wa') as outputfile:
+    outputcsv = csv.writer(outputfile)
+    with open("to-geocode", "r") as f:
+        for line in f.readlines()[:500]:
+            line = line.strip()
+            if line in pre_existing:
+                print 'Already have "%s"' % line
+                lat, lng = pre_existing[line]
+
+            else:
+                print line,
+                sys.stdout.flush()
+                lat, lng = get_lat_lng(line)
+                print lat, lng
+
+            if lat and lng:
+                outputcsv.writerow([line, lat, lng])
+            else:
+                print "OH NOES", lat, lng
---
+++ produce-geojson.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+
+## create a KML file from a (possily subset) of the collisions data,
+## given a "geocoded" file that is a CSV file consising of "address,
+## lat, lng" triples.
+
+import datetime
+import csv
+import geojson
+
+from collisions import load_collisions
+
+## load the geocoded data we have
+geocoded = {}
+with open('geocoded.csv', 'r') as f:
+    for (addr, lat, lng) in csv.reader(f):
+        geocoded[addr] = (lat, lng)
+
+with open('bikecollisions.csv', 'r') as f:
+    collisions = load_collisions(f)
+
+print len(collisions), "collisions loaded"
+
+years = set()
+for c in collisions:
+    years.add(c.startmonth[:4])
+years = map(int, list(years))
+years.sort()
+print "unique years %d -> %d" % (years[0], years[-1])
+
+for year in years:
+    features = []
+    i = 0
+    for collision in collisions:
+        if int(collision.startmonth[:4]) != year:
+            continue
+
+        addr = collision.address
+        if addr in geocoded:
+            lat, lng = geocoded[addr]
+            month = collision.startmonth[-2:]
+            month = datetime.date(int(year), int(month), 01).strftime('%B')
+
+            desc = 'Address: %s\n%s %s' % (collision.address, month, year)
+            p = geojson.Feature(geometry=geojson.Point((float(lat), float(lng))),
+                                properties=dict(name=addr,
+                                                description=desc),
+                                id=i)
+
+            features.append(p)
+            i += 1
+
+    if i > 0:
+        with open('collisions-%d.geojson' % year, 'w') as f:
+            f.write(geojson.dumps(geojson.FeatureCollection(features)))
